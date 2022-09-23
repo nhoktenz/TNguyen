0c96ceec931332e3edd4c8978c23a8dac830f64e
LouN <6723699+nhoktenz@users.noreply.github.com>
2022-09-22 23:01:35 -0400

    add border color

--- mod6/app.js
+++ mod6/app.js
@@ -9,6 +9,8 @@
   function LunchCheckController($scope) {
     $scope.lunch = "";
     $scope.result = "";
+    $scope.color = "";
+    $scope.borderColor = "black";
 
     $scope.checkLunch = function () {
       var items = $scope.lunch.split(",").filter((s) => s.trim());
@@ -17,12 +19,18 @@
         count += 1;
       }
 
-      if (count == 0) {
+      if (count === 0) {
         $scope.result = "Please enter data first";
+        $scope.color = "danger";
+        $scope.borderColor = "red";
       } else if (count >= 1 && count <= 3) {
         $scope.result = "Enjoy!";
+        $scope.color = "success";
+        $scope.borderColor = "green";
       } else {
         $scope.result = "Too much!";
+        $scope.color = "success";
+        $scope.borderColor = "green";
       }
     };
   }
