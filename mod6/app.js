34da6d9d065ff81ee2a70553ea3cd9d8f86e533b
LouN <6723699+nhoktenz@users.noreply.github.com>
2022-09-22 23:35:11 -0400

    add style.css

--- mod6/app.js
+++ mod6/app.js
@@ -10,8 +10,6 @@
     $scope.lunch = "";
     $scope.result = "";
     $scope.color = "";
-    $scope.borderColor = "";
-    $scope.border = "";
 
     $scope.checkLunch = function () {
       var items = $scope.lunch.split(",").filter((s) => s.trim());
@@ -23,18 +21,12 @@
       if (count === 0) {
         $scope.result = "Please enter data first";
         $scope.color = "danger";
-        $scope.borderColor = "red";
-        $scope.border = "border border-danger";
       } else if (count >= 1 && count <= 3) {
         $scope.result = "Enjoy!";
         $scope.color = "success";
-        $scope.borderColor = "green";
-        $scope.border = "border border-success";
       } else {
         $scope.result = "Too much!";
         $scope.color = "success";
-        $scope.borderColor = "green";
-        $scope.border = "border border-success";
       }
     };
   }
