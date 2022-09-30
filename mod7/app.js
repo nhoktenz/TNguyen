68747581eadfa56844851b1ea725b6fa7740601f
LouN <6723699+nhoktenz@users.noreply.github.com>
2022-09-30 02:53:00 -0400

    fix bug

--- mod7/app.js
+++ mod7/app.js
@@ -11,13 +11,18 @@
   function ToBuyController(ShoppingListCheckOffService) {
     var buyList = this;
 
-    buyList.items = ShoppingListCheckOffService.getToBuyItem();
-    console.log(ShoppingListCheckOffService.getToBuyItem());
+    buyList.items = ShoppingListCheckOffService.getToBuyItems();
+
+    buyList.buyItem = function (itemIndex) {
+      ShoppingListCheckOffService.buyItem(itemIndex);
+    };
   }
 
   AlreadyBoughtController.$inject = ["ShoppingListCheckOffService"];
   function AlreadyBoughtController(ShoppingListCheckOffService) {
     var boughtList = this;
+
+    boughtList.items = ShoppingListCheckOffService.getBoughtItems();
   }
 
   function ShoppingListCheckOffService() {
@@ -25,18 +30,26 @@
 
     //List of shopping items
     var toBuyItems = [
-      { name: "cookies", quantity: 10 },
-      { name: "cocacola", quantity: 5 },
-      { name: "energy-drinks", quantity: 3 },
-      { name: "snacks", quantity: 2 },
-      { name: "bags of noodle", quantity: 4 },
+      { name: "cookies", quantity: 10, pricePerItem: 1 },
+      { name: "can of cocacola", quantity: 5, pricePerItem: 2 },
+      { name: "bottles of energy-drinks", quantity: 3, pricePerItem: 2.5 },
+      { name: "bags of snacks", quantity: 2, pricePerItem: 2 },
+      { name: "bags of noodle", quantity: 4, pricePerItem: 3 },
     ];
 
     var boughtItems = [];
 
-    service.getToBuyItem = function () {
-      console.log(toBuyItems);
+    service.getToBuyItems = function () {
       return toBuyItems;
+    };
+
+    service.buyItem = function (itemIndex) {
+      boughtItems.push(toBuyItems[itemIndex]);
+      toBuyItems.splice(itemIndex, 1);
+    };
+
+    service.getBoughtItems = function () {
+      return boughtItems;
     };
   }
 })();
