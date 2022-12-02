f13eaa63c535577674349195413907a6c8a6e527
LouN <6723699+nhoktenz@users.noreply.github.com>
2022-12-02 09:50:56 -0500

    change heroku to firebase

--- mod9/src/menuapp/menudata.service.js
+++ mod9/src/menuapp/menudata.service.js
@@ -4,7 +4,10 @@
   angular
     .module("data")
     .service("MenuDataService", MenuDataService)
-    .constant("ApiBasePath", "https://davids-restaurant.herokuapp.com");
+    .constant(
+      "ApiBasePath",
+      " https://coursera-jhu-default-rtdb.firebaseio.com/"
+    );
 
   MenuDataService.$inject = ["$http", "ApiBasePath"];
   function MenuDataService($http, ApiBasePath) {
